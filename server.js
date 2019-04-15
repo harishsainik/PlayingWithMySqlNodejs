f881da187768fa61df7a26fd4b8d602bac59791b
Harish Saini <harish.saini@moglix.com>
2019-04-15 12:34:33 +0530

    Commit changes mysql DB

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log("First Statement");
+const mysql = require('mysql');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/config/config.json')[env];
+var connection = mysql.createConnection(config);
+
+connection.connect(()=>console.log("created connetion"));
+
+var query = connection.query("select * from customers",function(err,result){
+    console.log(query);
+    if (err){
+        return console.log(err);
+    }
+    console.log(result);  
+});
+let customer = {
+    id: "6",
+    name: 'User',
+    address: 'add of the user'
+};
+query = connection.query("insert into customers set ?", customer, (err, result) => {
+    if (err){
+
+        return console.log(err);
+    }else{
+        if (result.affectedRows == 1){
+            console.log("Data Inserted successfully");
+        }else
+            console.log("Affected rows: " + result.affectedRows);
+    }
+});
+
+
